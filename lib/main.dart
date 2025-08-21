0bd0da2cc999a2f17e25bc50553fb660aefa7cd7
= <=>
2025-08-21 18:40:32 +0530

    added container to center of the screen

--- lib/main.dart
+++ lib/main.dart
@@ -16,24 +16,26 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.green,
           title: const Text('Flutter is Fun'),
         ),
-        body: Container(
-          margin: const EdgeInsets.all(100),
-          padding: const EdgeInsets.all(20),
-          width: 100,
-          height: 100,
-          decoration: const BoxDecoration(
-            color: Colors.black87,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey,
-                offset: Offset(4, 4),
-                blurRadius: 10,
-              ),
-            ],
-          ),
-          child: const Text(
-            'hii mom @@',
-            style: TextStyle(color: Colors.white),
+        body: Center(
+          child: Container(
+            margin: const EdgeInsets.all(100),
+            padding: const EdgeInsets.all(20),
+            width: 100,
+            height: 100,
+            decoration: const BoxDecoration(
+              color: Colors.black87,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  offset: Offset(4, 4),
+                  blurRadius: 10,
+                ),
+              ],
+            ),
+            child: const Text(
+              'hii mom @@',
+              style: TextStyle(color: Colors.white),
+            ),
           ),
         ),
       ),
