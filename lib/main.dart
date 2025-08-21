9a1e247945064ec43a2b5446b234795509d9698a
= <=>
2025-08-21 18:38:47 +0530

    added MaterialApp with scaffold and with Container and shown title of the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
@@ -10,12 +11,32 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Scaffold(
+      home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.green,
           title: const Text('Flutter is Fun'),
         ),
-      )
+        body: Container(
+          margin: const EdgeInsets.all(100),
+          padding: const EdgeInsets.all(20),
+          width: 100,
+          height: 100,
+          decoration: const BoxDecoration(
+            color: Colors.black87,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                offset: Offset(4, 4),
+                blurRadius: 10,
+              ),
+            ],
+          ),
+          child: const Text(
+            'hii mom @@',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
     );
   }
 }
